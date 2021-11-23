819dc92181f818b14ff91b534ed6d70374359f63
thomas colligan <thomas.colligan@umontana.edu>
2021-11-23 13:38:11 -0700

    Change the simple train script to work with the new api

--- examples/train.py
+++ examples/train.py
@@ -3,44 +3,50 @@ import os
 import numpy as np
 from time import sleep
 from argparse import ArgumentParser
-from shopty import shopt_parser
+from shopty import ShoptyConfig
 
 
 def load_from_ckpt(path):
     with open(path, "r") as src:
         lines = src.read()
         s, i = lines.split(":")
-        print(lines)
     return s, i
 
 
 if __name__ == "__main__":
 
-    ap = shopt_parser()
+    ap = ArgumentParser()
+    ap.add_argument("--learning_rate", type=float)
+    args = ap.parse_args()
 
-    ap.add_argument("--random_seed", type=float)
+    shopty_config = ShoptyConfig()
 
-    args, unknown = ap.parse_known_args()
-    ckpt_path = os.path.join(args.experiment_dir, "checkpoint.txt")
-    print(f"experiment_dir {args.experiment_dir}, {ckpt_path}")
+    result_file = shopty_config.results_path
+    experiment_dir = shopty_config.experiment_directory
+    checkpoint_dir = shopty_config.checkpoint_directory
+    checkpoint_file = shopty_config.checkpoint_file
+    max_iter = shopty_config.max_iter
 
-    if args.load_from_ckpt:
-        start, increment = load_from_ckpt(ckpt_path)
+    print(result_file, experiment_dir, checkpoint_dir, checkpoint_file, max_iter)
+
+    if os.path.isfile(checkpoint_file):
+        start, increment = load_from_ckpt(checkpoint_file)
+        print(f"reloading at {start}, {increment}")
         start = float(start)
         increment = float(increment)
     else:
         start = np.random.rand() * 1000
-        increment = args.random_seed / 100
+        increment = args.learning_rate / 100
 
     i = 0
 
-    print("starting experiment...", start, increment)
-
-    while i < args.max_iter:
-        sleep(0.5)
+    while i < max_iter:
         start -= increment
         print(f"{i+1}: {start}")
         i += 1
 
-    with open(os.path.join(args.experiment_dir, "checkpoint.txt"), "w") as dst:
+    with open(checkpoint_file, 'w') as dst:
+        dst.write(f"{start}:{increment}")
+
+    with open(result_file, 'w') as dst:
         dst.write(f"{start}:{increment}")
