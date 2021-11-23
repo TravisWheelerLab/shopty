d187896e1e118f14daa4ed18d08beadf13dc55df
thomas colligan <thomas.colligan@umontana.edu>
2021-11-23 14:31:07 -0700

    Add CLI for the tool; remove examples/optim since it's replaced by the CLI

--- examples/optim.py
+++
@@ -1,8 +0,0 @@
-from shopty import Config, ExperimentGenerator, SlurmSupervisor, hyperband
-
-
-if __name__ == "__main__":
-
-    f = "hparams.yaml"
-    x = SlurmSupervisor(f, "slurm", poll_interval=10, monitor="max")
-    hyperband(x)
